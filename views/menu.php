80324821667b3e31a7a3684c45e3af86e456885e
FlorianLaronde <fllaronde@gmail.com>
2021-01-15 12:44:37 +0100

    Design terminé

--- views/menu.php
+++ views/menu.php
@@ -1,11 +1,11 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav id="navbar" class="fixed-top navbar navbar-expand-lg navbar-dark bg-dark">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav mx-auto">
 
             <!-- On boucle sur le tableau $website afvec la propriété page
             pour affucher chaque élément du menu -->
