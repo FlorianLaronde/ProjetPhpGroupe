dffe3939e3542b0a328e297b328a4705e760b1ff
kevinmilet <kevin.milet@gmail.com>
2021-01-15 09:25:25 +0100

    ajout de commentaires dans le code

--- views/menu.php
+++ views/menu.php
@@ -1,3 +1,4 @@
+<!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,9 +7,14 @@
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
 
+            <!-- On boucle sur le tableau $website afvec la propriété page
+            pour affucher chaque élément du menu -->
             <?php foreach($website->page as $key): ?>
-                <!-- on récupére l'id de la page pour afficher le menu -->
+
                 <li class="nav-item active">
+                    <!-- le href contient l'id de la page + l'extension .html 
+                    pour la redirection d'url (.htaccess)
+                    et on affiche l'élément de menu -->
                     <a class="nav-link" href="<?=$key['id']?>.html"><?=$key->menu?></a>
                 </li>
 
